5357f5bcd5b3922e5b6b48b10918cb8adcbb8cae
Dan ESPOSITO <espositodan95@gmail.com>
2020-01-07 23:03:57 +0100

    Tentative d'une structure d'histoire

---
+++ js/ClassHistoire.js
@@ -0,0 +1,79 @@
+class ClassHistoire {
+    constructor(){
+        this.intro="Née dans l'an 500 AP Jésus Christ dans la zone montagneuse des **Huangshan**, fils du forgeron Meiwong, et de Jido, femme couturière, dans une époque ou règnes dragons et sorciers.  \n" +
+            "\n" +
+            "L'école n'existait pas et les parents devaient garder eux-mêmes leurs enfants, c'est à la chaudronnerie ou travaille son père que Malao avec ses frères et sœurs grandissent. C'est donc dans la chaleur des fours, des chaudrons et sous le bruit du marteau claquant le métal froid des épées, haches, flèches que les enfants passent leur temps. \n" +
+            "\n" +
+            "Mais c'est à un autre destin que rêve le jeune garçon. Il passât son temps à regarder par la seule fenêtre les montagnes rocheuses de sa région. S'imaginant visiter chaque recoin à la découverte de lieux tout aussi fascinant les uns que les autres. Mais son père le rappela à l'ordre chaque jour lui en disant que le monde extérieur est peuplé de dragons et de sorciers maléfiques.  \n" +
+            "\n" +
+            "Comme dans chaque famille, il existe toujours un enfant différent des autres. Malao était celui-là. Il travailla avec son père et ses frères en tant que chaudronnier jusqu'à ses 14ans.  C'est à partir de 15 ans qu'il s'enfuit de la chaudronnerie la journée pour visiter les montagnes de sa région et choisi alors son destin : celui d'aventurier.";
+        this.choix={"e1c1":["Malao décide de tout raconter à son père","e1c1"],
+            "e1c2":["Malao décide de mentir","e1c2"]
+        };
+        this.texte={
+            "un":"Un beau jour de soleil, pendant une virée en montagne, Malao s'enfonça dans une grotte de plus en plus sombre, froide et humide. Les dragons faisaient généralement leur nid dans de ce genre d'endroit à l'abris des chausseurs. Petit pas par petit pas il continue son chemin, son souffle et son poux sont de plus en plus fort. Malao ne voyait rien, seule la main sur le mur pouvait l'aider à avancer et à se situer dans la grotte... \n" +
+                "\n" +
+                "Mais, ne pouvant pas voir les obstacles sur le sol, il trébuchât sur une roche en hurlant et en se cognant la tête. C'est alors qu'un bruit sourd retentit. Pris de panique Malao se relève en trompe et rebrousse chemin jusqu'à la chaudronnerie pour retrouver son père. Inquiet, son père lui demande ce qu'il s'est passé :",
+            "deuxMort":"celui-ci l'engueula et le prive de sorties. Il travaille alors de nouveau à la chaudronnerie et une idée s'illumine dans sa tête : se construire une dague, lui servant à se protéger de ses virées externes. Des semaines après sa chute et son retour à la chaudronnerie, le dernier coup de marteaux ressentit sur sa dague. C'est dans un fourreau de bois que la lame ondulée fut couplée. \n" +
+                "\n" +
+                "    Le lendemain il décide de sortir de nouveau sous le désespoir de son père. Une fois la nuit tombé et Malao rentré, son père l'amena voir un ancien combattant. Son père ayant déjà aidé cette personne dans le temps, il lui demanda de prendre son fils sous son aile pour lui apprendre l'art du combat. \n" +
+                "\n" +
+                "    Malheureusement, Malao avait une tout autre idée que de venir s'entrainer au combat tous les jours. Seule l'envi de revenir dans la grotte trotta dans sa tête. Alors que son père le laissa seul aller à l'entrainement un jour de pluie, le vieux combattant annula la séance quand Malao vient frapper à sa porte. Il en profita pour retourner voir dans la grotte...   \n" +
+                "    Tout doucement il s'enfonça de nouveau dans la grotte, avec comme seul défense les quelques courts reçus par le vieillard et sa dague. Petit à petit il sentit une chaleur et ressentit une sensation, comme si quelqu'un ou quelque chose l'observé. Malencontreusement pour lui, ses pieds tapaient dans quelque chose de dur, c'est avec sa main qu'il découvrit quelque chose de dur, écailleux, chaud face à lui. Il recule, puis vit s'ouvrir devant lui deux yeux jaunes...\n" +
+                "\n" +
+                "    Il recula mais c'était déjà trop tard pour lui, le dragon avait glissé sa queue au niveau de ses pieds et le fit malencontreusement tomber sur le sol. Apeuré, il donne des grands coups de dague dans le vide tout en reculant sur le sol, le dragon s'approcha de plus en plus de lui puit finit par le manger... \n" +
+                "\n" +
+                "    Malao meurt...",
+            "deux":"et raconte qu'il est tombé sur une roche pour pouvoir sortir de nouveau le lendemain sans subir la colère de son père. \n" +
+                "   Il décide alors de se reposer et de travailler à la chaudronnerie. Des semaines après des semaines, sous la chaleur bouillante, son père lui dit \"demain tu ne viens pas travailler, je vais t'amener voir une personne\". Surprit de cette décision, le stresse s'empare de son esprit, qui allons-nous voir se demanda Malao. Impossible de lui pour dormir il attend patiemment le lendemain. \n" +
+                "   Le jour se lève, son père entre dans la chambre, il sait qu'il doit le suivre. Seul sa mère sait de quoi il s'agit. A table pour le petit déjeuner, le père raconte à son fils :  \n" +
+                "   *\"Malao, tu as maintenant 17 ans, il est l'heure pour toi d'aller voir une personne qui pourra t'apprendre l'art du combat.\"*   \n" +
+                "   C'est sous un ciel caché par les nuages que Malao suivit son père le long d'un sentier à la sortie du village. Ils arrivent face à une vieille maison, son père tape et une vieille voix cassée lui ordonne de rentrer. Ils rentrent et Malao se retrouve en présence d'un vieux petit homme à la barbe longue et au regard creusé. Il observe l'intérieur de la maison et remarque une multitude d'armes blanches sur les murs. C'est alors que son père serra le vieil homme contre lui, comme un signe d'une amitié lointaine. Il murmure conjointaient à leurs oreilles. \n" +
+                "\n" +
+                "   Les deux hommes posent leurs regards sur Malao, son père lui explique alors que c'est un vieil ami à lui et que c'est un ancien combattant qui va le prendre sous son aile pour lui transmettre différentes techniques d'art martiaux. Son père sort alors une épée de sa veste et la donne à Malao en lui disant que c'est l'arme de son grand père et d'en prendre soin.\n" +
+                "Le premier jour avec le vieil homme fut spécial pour Malao, le vieil homme ne dit rien et il reste tout deux sur les chaises, comme si le vieillard voulait voir la réaction du jeune homme sans discuter, juste en observant. Le soleil tombe de plus en plus et il dit alors au jeune homme *\"rentre chez toi et sois la demain à l'aube\"*. \n" +
+                "\n" +
+                "Excité, il rentre chez lui en courant et serre son père dans les bras, ainsi que sa mère, il mange et se couche pour être en forme le lendemain. Dès les premiers rayons de soleil posés sur son visage, il saute du lit, gobe son petit déjeuner et enfile ses habits pour courir voir le vieillard. Une fois arrivé, il le vit debout dehors et lui fit un signe de main lui indiquant de le suivre derrière la maison. \n" +
+                "\n" +
+                "Son premier entrainement fut basé sur le combat à main nue, Malao râle et subit un coup de bâton par le vieillard qui lui expliqua :  \n" +
+                "\" Comment veux-tu te battre avec une épée si tu ne sais même pas manier tes mains ?\"  \n" +
+                "Celui-ci lui montra un mannequin en bois et lui ordonna de le frapper toute la journée. Bête comme il est, Malao donna tout ce qu'il a dès le début et fit essouffler, épuisé, faible au bout de 5 minutes...  \n" +
+                "Le vieillard lui dit de recommencer mais Malao recommence encore une fois. Encore un coup de bâton, le vieillard lui indique de s’assoir en lotus sur le banc à ses côtés.",
+            "trois":""
+
+        };
+        this.embranchementZero();
+    }
+
+    embranchementZero(){
+        $("main").append("<section></section>");
+        $('main section').append(`<h1>${this.intro}</h1>`);
+        $('main section').append("<button class='debut'>Début</button>");
+        $(".debut").click(( event => {
+            this.clear();
+            this.embranchementUn();
+        }));
+    }
+
+    embranchement(texte,classeUn,classedeux,phraseButonUn,PhraseButonDeux){
+        $('main section').append(`<p>${this.texte.un}</p>`);
+        this.generateBouton(`${this.choix.e1c1[1]}`,`${this.choix.e1c2[1]}`,`${this.choix.e1c1[0]}`,`${this.choix.e1c1[0]}`);
+    }
+    clear(){
+        $('main section').empty();
+    }
+    generateBouton(classeUn,classeDeux,phraseBoutonUn,phraseBoutonDeux){
+            $('main section').append(`<button class="${classeUn}">${phraseBoutonUn}</button><button class="${classeDeux}">${phraseBoutonDeux}</button>`);
+            this.clicBoutonChoix()
+    }
+    clicBoutonChoix(){
+        $("main section button").click(( event => {
+            if(event.target.className==="e1c2"){
+
+            }
+            if(event.target.className==="e1c1"){
+
+            }
+        }));
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,2 @@
+$("main").append("<h1>Le voyage d'un petit asiatique</h1>");
+let histoire = new ClassHistoire();
